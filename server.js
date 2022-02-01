fbb048655e9ce0566c6c9d631527dbf2f47c61b2
Iyal Khanjar <ayalkhanjar@gmail.com>
2022-02-01 13:02:10 +0200

    changes

--- server.js
+++ server.js
@@ -28,12 +28,6 @@ app.get('/', (req, res) => {
     res.send('welcome')
 })
 
-// path
-// const path = require('path')
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
 // middleware
 app.use(notFound);
 app.use(errorHandler);
