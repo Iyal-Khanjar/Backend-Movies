f44727590fabc059a041dd1e323373bbfef3ed56
asad hmeed <85388826+newasad@users.noreply.github.com>
2022-02-13 13:39:04 +0200

    Revert "Web scrapper"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const userRouter = express.Router()
 const { registerUser, authUser, deleteUser, updateUserProfile, allUsersProfile, googleLogin } = require("../controllers/userController.js")
-const { bestActors , funniestActors,bestMovie } = require("../controllers/dataController.js")
 const { protect } = require('../middlewares/authMiddleware')
-// const {getBestMovie} =require('../scraper/bestMovies')
 
 userRouter.route('/allusersprofile').get(allUsersProfile)
 userRouter.route('/register').post(registerUser)
@@ -11,9 +9,6 @@ userRouter.route('/signin').post(authUser)
 userRouter.route('/google-login').post(googleLogin)
 userRouter.route("/profile/").post(protect, updateUserProfile);
 userRouter.route('/:id').delete(deleteUser)
-userRouter.route('/bestActors').get(bestActors)
-userRouter.route('/funniestActors').get(funniestActors)
-userRouter.route('/bestMovies').get(bestMovie)
 
 
 
